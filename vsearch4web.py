bb8c55a8acd819a053048d29576dc054e7dd7efb
carlstep <carlstep333@me.com>
2021-03-08 08:46:34 +0100

    update code to improve log file

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch2 import search4letters
 
 app = Flask(__name__)
+
+# function to capture and log the results
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req.form, file=log)
+        print(req.remote_addr, file=log)
+        print(req.user_agent, file=log)
+        print(res, file=log)
+        #print(str(dir(req)), res, file=log)
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results: '
-    results = str(search4letters(phrase, letters))
+    results = str(search4letters(phrase, letters)) 
+    log_request(request, results) # appends the results of each web 'request' to log_requests file
     return render_template('results.html',
                            the_phrase=phrase,
                            the_letters=letters,
@@ -20,6 +30,12 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return escape(contents)
 
 if __name__ == '__main__':
     app.run(debug=True)
